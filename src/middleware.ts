913cbcb39c9117e44d656d82a04b47d2a08c62bd
alielabdo <alloyali48@gmail.com>
2025-05-22 20:44:23 +0300

    line16 line20

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,11 +13,11 @@ export default async function middleware(req: NextRequest) {
   const userRole = session.role;
   
   if (url.startsWith("/cus_dashboard") && userRole !== "CUSTOMER") {
-    return NextResponse.redirect(new URL("/", req.nextUrl.origin));
+    return NextResponse.redirect(new URL("/des_dashboard", req.nextUrl.origin));
   }
 
   if (url.startsWith("/des_dashboard") && userRole !== "DESIGNER") {
-    return NextResponse.redirect(new URL("/", req.nextUrl.origin));
+    return NextResponse.redirect(new URL("/cus_dashboard", req.nextUrl.origin));
   }
 
   return NextResponse.next();
